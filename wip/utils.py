7bb201fcbe7542f3534f5c8d4a4b0fccaee72e99
Lonneke Langeveld <l.langeveld@gmail.com>
2022-02-08 16:43:58 +0100

    🛠️ Update to utils.py

--- wip/utils.py
+++ wip/utils.py
@@ -1,32 +1,6 @@
 import langid
-
-
-def load_full_files():
-    """
-    Loads the six full data files, and returns them as lists.
-    :return: The six full data files (as lists)
-    """
-    EMEA_en_f = open("/home/lonneke/thesis/local/data/"
-                     "corrected_data/EMEA/full.en")
-    EMEA_de_f = open("/home/lonneke/thesis/local/data/"
-                     "corrected_data/EMEA/full.de")
-    GNOME_en_f = open("/home/lonneke/thesis/local/data/"
-                      "corrected_data/GNOME/full.en")
-    GNOME_de_f = open("/home/lonneke/thesis/local/data/"
-                      "corrected_data/GNOME/full.de")
-    JRC_en_f = open("/home/lonneke/thesis/local/data/"
-                    "corrected_data/JRC/full.en")
-    JRC_de_f = open("/home/lonneke/thesis/local/data/"
-                    "corrected_data/JRC/full.de")
-
-    EMEA_en = [s for s in EMEA_en_f]
-    EMEA_de = [s for s in EMEA_de_f]
-    GNOME_en = [s for s in GNOME_en_f]
-    GNOME_de = [s for s in GNOME_de_f]
-    JRC_en = [s for s in JRC_en_f]
-    JRC_de = [s for s in JRC_de_f]
-
-    return EMEA_en, EMEA_de, GNOME_en, GNOME_de, JRC_en, JRC_de
+from presidio_analyzer.nlp_engine import NlpEngineProvider
+from presidio_analyzer import AnalyzerEngine
 
 
 def get_all_texts(domain: str, language: str):
@@ -40,17 +14,31 @@ def get_all_texts(domain: str, language: str):
     :return: Three lists: train, test, and valid
     """
     train = open(f"/home/lonneke/thesis/local/data/"
-                 f"full_sent_datasets/{domain}/train.{language}")
+                 f"corrected_data/{domain}/train.{language}")
     test = open(f"/home/lonneke/thesis/local/data/"
-                f"full_sent_datasets/{domain}/test.{language}")
+                f"corrected_data/{domain}/test.{language}")
     valid = open(f"/home/lonneke/thesis/local/data/"
-                 f"full_sent_datasets/{domain}/valid.{language}")
+                 f"corrected_data/{domain}/valid.{language}")
 
     train_l = [s for s in train]
     test_l = [s for s in test]
     valid_l = [s for s in valid]
 
     return train_l, test_l, valid_l
+
+
+def load_full_files():
+    """
+    Loads the six full data files, and returns them as lists.
+    :return: The six full data files (as lists)
+    """
+    d = {}
+    for domain in ["EMEA", "GNOME", "JRC"]:
+        for lang in ["en", "de"]:
+            x1, x2, x3 = get_all_texts(domain, lang)
+            d[f"{domain}_{lang}"] = x1 + x2 + x3
+
+    return d
 
 
 def remove_wrong_language(text_en: list, text_de: list):
@@ -65,3 +53,24 @@ def remove_wrong_language(text_en: list, text_de: list):
             new_de.append(de)
 
     return new_en, new_de
+
+
+def create_presidio_engine():
+    print("Building engine...")
+    # Create configuration containing engine name and models
+    configuration = {
+        "nlp_engine_name": "spacy",
+        "models": [{"lang_code": "de", "model_name": "de_core_news_lg"},
+                    {"lang_code": "en", "model_name": "en_core_web_lg"}],
+    }
+    # Create NLP engine based on configuration
+    provider = NlpEngineProvider(nlp_configuration=configuration)
+    nlp_engine= provider.create_engine()
+
+    # Pass the created NLP engine and supported_languages to the AnalyzerEngine
+    analyzer = AnalyzerEngine(
+        nlp_engine=nlp_engine,
+        supported_languages=["en", "de"]
+    )
+
+    return analyzer
