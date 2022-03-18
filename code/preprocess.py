70f9aeea37779825b7ba2a88c9a088cd44d23e79
Lonneke Langeveld <l.langeveld@gmail.com>
2022-03-18 14:58:50 +0100

    ✨ Build preprocess.py to make JSON-files

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,29 +1,38 @@
-f = open("../data/opus-test/gnome.out")
+import json
 
-doc_num = 0
-en = []
-de = []
-docs = {}
-prev = ""
-for s in f:
-    s = s.strip()
-    if not s:
-        continue
-    elif s[0] == "#":
-        if prev != "#":
-            docs[f"{doc_num}"] = {"en": en, "de" : de}
-            print(en)
-            print(de)
+def get_domain_dict(text):
+    doc_num = 0
+    en = []
+    de = []
+    docs = {}
+
+    for i, s in enumerate(text):
+        s = s.strip()
+        if (s.startswith("# de") and en and de) or (i == len(text) - 1):
+            docs[f"{doc_num}"] = {"en": en, "de": de}
             doc_num += 1
             en = []
             de = []
-    elif s[0] == "=":
-        continue
-    else:
-        if s.startswith("(src)"):
+        elif s.startswith("(src)"):
             idx = s.find('>')
-            de.append(s[idx+1:])
+            de.append(s[idx + 1:])
         elif s.startswith("(trg)"):
             idx = s.find('>')
-            en.append(s[idx+1:])
-    prev = s[0]
+            en.append(s[idx + 1:])
+        else:
+            continue
+
+    return docs
+
+
+
+def main():
+    for domain in ["EMEA", "GNOME", "JRC"]:
+        f = open(f"../data/opus/test/{domain}.out")
+        text = [s for s in f]
+        docs = get_domain_dict(text)
+        with open(f"../data/opus/json/{domain}.json", "w") as outfile:
+            json.dump(docs, outfile, indent=2)
+
+if __name__ == "__main__":
+    main()
