c162965bfbb3b69a3f35586b0278218a5b2e934b
Lonneke Langeveld <l.langeveld@gmail.com>
2022-03-24 13:00:42 +0100

    ✨ Add main.py for main pipeline

---
+++ code/main.py
@@ -0,0 +1,19 @@
+from utils import make_language_files
+from ngram import calculate_tfidf
+from eval import evaluate_bleu
+
+
+def main():
+    for domain in ["EMEA", "GNOME", "JRC"]:
+        d = {}
+        d["en"], d["de"] = make_language_files(domain, max_docs=3)
+        for language in ["en"]:
+            docs = d[f"{language}"]
+            new_docs = calculate_tfidf(docs, language)
+
+            for doc, new_doc in zip(docs, new_docs):
+                evaluate_bleu(doc, new_doc)
+
+
+if __name__ == "__main__":
+    main()
