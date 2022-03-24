ff6359ede8e8c374d3507462811bb625a5905950
Lonneke Langeveld <l.langeveld@gmail.com>
2022-03-24 12:42:46 +0100

    ⬆️ eval.py to be used in other files instead of stand-alone

--- code/eval.py
+++ code/eval.py
@@ -1,43 +1,26 @@
-from utils import load_full_files
 from sacrebleu import BLEU
-import anonymize
-import random
-
-print("Initializing engines...")
-engine = anonymize.create_presidio_analyzer()
-anonymizer = anonymize.create_presidio_anonymizer()
-bleu = BLEU(effective_order=True)
-NUM_SENTS = 150
 
 
-def do_analysis(text: list, anon_text: list) -> int:
+def do_analysis(text: list, anon_text: list, bleu) -> int:
     found = 0
-    idxs = random.sample(range(len(text)-1), NUM_SENTS)
-    new_text = [text[i] for i in idxs]
-    for i in idxs:
+    # Go over every sentence in the anonymized text
+    for idx, sentence in enumerate(anon_text):
         max_idx = 0
         max_score = 0
-        for idx, s in enumerate(new_text):
-            score = bleu.sentence_score(anon_text[i], [s]).score
+        # Go over every sentence in the regular text
+        for i, s in enumerate(text):
+            score = bleu.sentence_score(sentence, [s]).score
             if score > max_score:
                 max_score = score
-                max_idx = idx
-
-        if new_text[max_idx] == text[i]:
+                max_idx = i
+        if text[max_idx] == text[idx]:
             found += 1
 
     return found
 
 
-def main():
-    print("Starting program...")
-    d = load_full_files()
-    for k, v in d.items():
-        sp_text = anonymize.anonymize_text_spacy(v.strip(), k[-2:])
-        f = do_analysis(v, sp_text)
-        ratio = f/NUM_SENTS
-        print(f"Results for {k}: {ratio}")
-
-
-if __name__ == "__main__":
-    main()
+def evaluate_bleu(text: list, anon_text: list):
+    bleu = BLEU(effective_order=True)
+    f = do_analysis(text, anon_text, bleu)
+    ratio = f/len(text)
+    print(f"BLEU-results: {ratio}")
