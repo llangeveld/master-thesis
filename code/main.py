9e2fdc455edfb9a70044ffcde6fd13b8f03f8ec9
Lonneke Langeveld <l.langeveld@gmail.com>
2022-04-01 12:33:19 +0200

    ⬆️ Add other domains to main.py

--- code/main.py
+++ code/main.py
@@ -4,14 +4,13 @@ from eval import evaluate_fscore
 
 
 def main():
-    for domain in ["EMEA"]:
+    for domain in ["EMEA", "GNOME", "JRC"]:
         d = {}
         d["en"], d["de"] = make_language_files(domain, max_docs=2)
         for language in ["en", "de"]:
             docs = d[f"{language}"]
             new_docs = calculate_tfidf(docs, language, "spacy")
             for doc, new_doc in zip(docs, new_docs):
-                # evaluate_bleu(doc, new_doc)
                 evaluate_fscore(doc, new_doc)
 
 
