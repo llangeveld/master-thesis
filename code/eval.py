2c0fa2aebe8b463203b16e9ff81a7452a59a0f34
Lonneke Langeveld <l.langeveld@gmail.com>
2022-04-01 12:29:29 +0200

    ⬆️ Update on evaluating BLEU

--- code/eval.py
+++ code/eval.py
@@ -5,20 +5,24 @@ bleu = BLEU(effective_order=True)
 model = SentenceTransformer("distiluse-base-multilingual-cased-v1")
 
 
-def do_analysis(text: list, anon_text: list) -> int:
+def evaluate_bleu(text: list, anon_text: list) -> float:
     """
-    Find the number of sentences that were not properly anonymized
+    Returns the ratio of sentences that were properly anonymized
     :param text: List of unanonymized sentences
     :param anon_text: Parallel list of anonymized sentences
     :return: Number of sentences that were not properly anonymized (int)
     """
     found = 0
+    empty = 0
     # Go over every sentence in the anonymized text
     for idx, sentence in enumerate(anon_text):
         max_idx = 0
         max_score = 0
         # Go over every sentence in the regular text
         for i, s in enumerate(text):
+            if not s:
+                empty += 1
+                continue
             score = bleu.sentence_score(sentence, [s]).score
             if score > max_score:
                 max_score = score
@@ -26,23 +30,11 @@ def do_analysis(text: list, anon_text: list) -> int:
         if text[max_idx] == text[idx]:
             found += 1
 
-    return found
-
-
-def evaluate_bleu(text: list, anon_text: list):
-    """
-    Returns the ratio of sentences that were not properly anonymized
-    :param text: List of unanonymized sentences
-    :param anon_text: Parallel list of anonymized sentences
-    :return: None
-    """
-    f = do_analysis(text, anon_text)
-    ratio = f / len(text)
-    print(f"BLEU-results: {ratio}")
+    ratio = found / (len(text) - empty)
     return 1 - ratio
 
 
-def evaluate_semsim(text: list, anon_text: list):
+def evaluate_semsim(text: list, anon_text: list) -> float:
     sim_scores = []
     for before, after in zip(text, anon_text):
         embed_before = model.encode(before, convert_to_tensor=True)
@@ -53,9 +45,11 @@ def evaluate_semsim(text: list, anon_text: list):
     return score
 
 
-def evaluate_fscore(text: list, anon_text: list):
+def evaluate_fscore(text: list, anon_text: list) -> None:
     bleu_score = evaluate_bleu(text, anon_text)
     semsim_score = evaluate_semsim(text, anon_text)
     # f1 = 2 * ((bleu_score * semsim_score) / (bleu_score + semsim_score))
     avg = (bleu_score+semsim_score)/2
-    print(f"F1-score: {avg}")
+    print(f"BLEU-score: {bleu_score}\n"
+          f"Semantic score: {semsim_score}\n"
+          f"F1-score: {avg}")
