eb589e11e0c33084fff9b8b6b3a34341ce398e64
Lonneke Langeveld <l.langeveld@gmail.com>
2022-06-02 14:17:29 +0200

    ⬆️ Update interactive.sh

--- code/shell/interactive.sh
+++ code/shell/interactive.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 START=/data/s3225143
-DATA=$START/data/4_translation/processed/EMEA.de-en/
-MODELS=$START/models
-THISMODEL=$MODELS/wmt19.de-en.joined-dict.ensemble
-PRETRAINED=$THISMODEL/model1.pt
-BPECODES=$THISMODEL/bpecodes
-SAVE=$MODELS/finetune/de-en/EMEA/
+DATA=$START/data/1_main_data/EMEA/test.en
+DATA_TRG=$START/data/1_main_data/EMEA/test.de
+MODEL_DIR=$START/models/finetune/en-de/EMEA/
 
-CUDA_VISIBLE_DEVICES=0 fairseq-train $DATA \
-  --keep-best-checkpoints 1 --save-interval 100  \
-  --keep-interval-updates 1  --keep-last-epochs 1 \
-  --keep-best-checkpoints 1   --save-interval-updates 500 \
-  --no-epoch-checkpoints \
-  --no-save-optimizer-state \
-  --no-progress-bar --log-format json --log-interval 100 --log-file $SAVE/log.out\
-  --finetune-from-model $PRETRAINED --task translation \
-  --arch transformer_wmt_en_de_big --source-lang de --target-lang en \
-  --save-dir $SAVE \
-  --eval-bleu \
-  --eval-bleu-detok moses \
-  --eval-bleu-remove-bpe \
-  --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
-  --ddp-backend c10d --max-tokens 4096 --max-tokens-valid 4096 --update-epoch-batch-itr True \
-  --max-epoch 20 --update-freq 1 \
-  --criterion label_smoothed_cross_entropy --label-smoothing 0.1 --ignore-prefix-size 0 \
-  --optimizer adam --adam-betas '(0.9, 0.98)' --adam-eps 1e-08 --relu-dropout 0.0 --dropout 0.2 \
-  --weight-decay 0.0001 --attention-dropout 0.01 \
-  --lr-scheduler inverse_sqrt --lr 0.00017 --warmup-updates 4000 --warmup-init-lr 1e-07 --stop-min-lr 1e-09 \
-  --bpe fastbpe --bpe-codes $BPECODES \
+fairseq-interactive \
+  --path $MODEL_DIR/checkpoint_best.pt $MODEL_DIR \
+  --source-lang en --target-lang de \
   --tokenizer moses \
-  --decoder-attention-heads 16 --decoder-embed-dim 1024 --decoder-ffn-embed-dim 4096 \
-  --decoder-layers 6 --decoder-output-dim 1024 \
-  --encoder-attention-heads 16 --encoder-embed-dim 1024 --encoder-ffn-embed-dim 8192 --encoder-layers 6 \
-  --share-decoder-input-output-embed \
+  --input "$DATA" \
+  --beam-size 1
