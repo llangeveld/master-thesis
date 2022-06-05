d1ba5c3507201439ef7625d6dfcefd21f00b9eef
Lonneke Langeveld <l.langeveld@gmail.com>
2022-06-05 10:06:36 +0200

    ⬆️ Detokenize sentences for finetuning model for anonymized sentences

--- code/postprocess_data.py
+++ code/postprocess_data.py
@@ -12,6 +12,13 @@ def main():
                 with open(f"../data/1_main_data/detok/{domain}/{phase}.{language}", "w") as f:
                     for s in detoks:
                         f.write(s + "\n")
+            for pair in ["de-en", "en-de"]:
+                x = open(f"../data/3_anonymized/full/{domain}.{pair}.{language}")
+                x_list = [s for s in x]
+                anon_detoks = [md.detokenize(s.split()) for s in x_list]
+                with open(f"../data/3_anonymized/detok/{domain}.{pair}.{language}", "w") as z:
+                    for s in anon_detoks:
+                        z.write(s)
 
 
 if __name__ == "__main__":
